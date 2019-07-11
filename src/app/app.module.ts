ebd9e7ef3cc6d6d3b7a38f4277ea23472ec2aaca
Mustafa Awwal <mustafaawwal1@gmail.com>
2019-07-11 14:43:27 +0500

    components update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,20 @@ import { ReactiveFormsModule,FormsModule} from '@angular/forms';
 // Imports for http Services
 import { HttpClientModule } from '@angular/common/http'
 
-//Router Module
+// Router Module
 import { AppRoutingModule} from './routerModule'
 
-//Font Awesome 5 module 
+// Font Awesome 5 module 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 
-//These imports are for the icon 
+// These imports are for the icon 
 import { faHistory,faSlidersH,faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { faWindowMaximize, faUser } from '@fortawesome/free-regular-svg-icons';
 
-//Imports for Animations 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+// Imports for Animations 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
--- src/app/components/pages/admin/admin.module.ts
+++ src/app/components/pages/admin/admin.module.ts
@@ -5,10 +5,32 @@ import { adminRoutes } from './admin.routes';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { SideNavComponent } from '../../UI/side-nav/side-nav.component';
 
+// Font Awesome 5 module 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// These imports are for the icon
+import { faHistory, faSlidersH, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faWindowMaximize, faUser } from '@fortawesome/free-regular-svg-icons';
+
 
 @NgModule({
     declarations: [AdminDashboardComponent, SideNavComponent],
-    imports: [CommonModule,RouterModule.forChild(adminRoutes)]
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+        RouterModule.forChild(adminRoutes)
+    ]
 })
 
-export class AdminModule {}
+export class AdminModule {
+    constructor(){
+        library.add(
+            faHistory,
+            faSlidersH,
+            faUserCircle,
+            faWindowMaximize,
+            faUser
+        )
+    }
+}
